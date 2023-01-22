b70142c09c7d381ca1ccfee0dfd90b0365834f04
Elena1646 <122553809+Elena1646@users.noreply.github.com>
2023-01-22 19:39:04 +0100

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,12 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+ from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+_next > form > div > div.MuiCardContent-root > h5
+ login_field_xpath = "//*[@id='login']"
+password_field_xpath =
+_next > form > div > div.MuiCardContent-root > a
+sign_in_button_xpath =
+_next > form > div > div.MuiCardActions-root > div
+def type_in_email(self, email):
+  self.field_send_keys(self.login_field_xpath, email)
