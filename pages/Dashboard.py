e91a82dcceb70b9336c74846278b98e7ce3b6f7a
Elena1646 <122553809+Elena1646@users.noreply.github.com>
2023-01-24 19:01:57 +0100

    Create Dashboard.py

---
+++ pages/Dashboard.py
@@ -0,0 +1,18 @@
+import span as span
+
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+button_xpath = "//*[@id = 'login"
+//*[@id="__next"]/div[1]/div/div/div/ul[1]/div[1]/span
+//*[@id="__next"]/div[1]/div/div/div/ul[1]/div[2]
+//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[1]
+//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[2]
+//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[2]/div[1]
+//*[@id="__next"]/div[1]/main
+//*[@id="__next"]/div[1]/main/div[2]/div[1]/div
+//*[@id="__next"]/div[1]/main/div[2]/div[2]
+//*[@id="__next"]/div[1]/main/div[2]/div[3]
+//*[@id="__next"]/div[1]/main/div[2]/div[4]
+//*[@id="__next"]/div[1]/main/div[3]
